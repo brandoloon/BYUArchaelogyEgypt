e002a73a4ee817416186ae7c4fb28957b68ed3d9
Preston Loveland <62196792+prestonloveland@users.noreply.github.com>
2021-04-08 09:29:39 -0600

    Added C14 Data to model

--- Models/BiologicalSample.cs
+++ Models/BiologicalSample.cs
@@ -32,6 +32,33 @@ namespace BYUArchaeologyEgypt.Models
 
         public string Area { get; set; }
 
+        //Added C14 Data into the Biological Samples table becaue we figure you can't take any Carbon data on something thats not a biological sample
+        //Each sample has the details of it and possibility for C14 data for it
+
+        public float Size { get; set; }
+
+        public int Foci { get; set; }
+
+        public int C14_2017 { get; set; }
+
+        public string Location { get; set; }
+
+        public string Questions { get; set; }
+
+        public int ConventionalC14Age { get; set; }
+
+        public int C14CalendarDate { get; set; }
+
+        public int CalendarDateMAX { get; set; }
+
+        public int CalendarDateMIN { get; set; }
+
+        public int CalendarDateSPAN { get; set; }
+
+        public int CalendarDateAVG { get; set; }
+
+        public string Category { get; set; }
+
 
     }
 }
