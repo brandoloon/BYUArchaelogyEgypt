e41b4a9138ff210979df53ce7b5343ada4f12132
Preston Loveland <62196792+prestonloveland@users.noreply.github.com>
2021-04-10 12:57:46 -0600

    added filtering

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,6 +10,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using System.IO;
 using BYUArchaeologyEgypt.Views.ViewModels;
+using Microsoft.EntityFrameworkCore;
 
 namespace BYUArchaeologyEgypt.Controllers
 {
@@ -29,17 +30,51 @@ namespace BYUArchaeologyEgypt.Controllers
             return View();
         }
 
-        public IActionResult BurialList(long? categoryId, int pageNum = 1)
+        public IActionResult BurialList(string id, int pageNum = 1)
         {
             int pageSize = 5;
-            //ViewData["location"] = _BurialContext.Locations.Where(i => i.Id == item.Location).FirstOrDefault();
             ViewData["locationList"] = _BurialContext.Locations.ToList();
+            var filters = new Filters(id);
+            ViewBag.Filters = filters;
+            ViewBag.HairColors = _BurialContext.Burials.Select(x => x.Hair_color).Distinct().ToList();
+            ViewBag.BurialWrapping = _BurialContext.Burials.Select(x => x.Burial_wrapping).Distinct().ToList();
+            ViewBag.AgeBracketAtDeath = _BurialContext.Burials.Select(x => x.Age_bracket_at_death).Distinct().ToList();
+            ViewBag.Sex = _BurialContext.Burials.Select(x => x.Sex).Distinct().ToList();
+            ViewBag.BonesCollected = new List<string> { "True", "False" };
 
+
+            IQueryable<Burial> query = _BurialContext.Burials;
+
+            if (filters.HasHairColor)
+            {
+                query = query.Where(t => t.Hair_color == filters.HairColorId);
+            }
+            if (filters.HasBurialWrapping)
+            {
+                query = query.Where(t => t.Burial_wrapping == filters.BurialWrapping);
+            }
+            if (filters.HasAgeBracket)
+            {
+                query = query.Where(t => t.Age_bracket_at_death == filters.AgeBracketAtDeath);
+            }
+            if (filters.HasSex)
+            {
+                query = query.Where(t => t.Sex == filters.Sex);
+            }
+            if (filters.HasBonesCollected)
+            {
+                query = query.Where(t => t.Bone_taken == ("true" == filters.BonesCollected) );
+            }
+            if (filters.HasYearFound)
+            {
+                query = query.Where(t => t.Year_found.ToString() == filters.YearFound);
+            }
+
+            var tasks = query.ToList();
 
             return View(new BurialListViewModel
             {
-                Burials = (_BurialContext.Burials
-                .Where(b => b.Estimated_age_at_death == categoryId || categoryId == null)
+                Burials = (query
                 .OrderBy(x => x.BurialID)
                 .Skip((pageNum - 1) * pageSize)
                 .Take(pageSize)
@@ -49,12 +84,21 @@ namespace BYUArchaeologyEgypt.Controllers
                 {
                     NumItemsPerPage = pageSize,
                     CurrentPage = pageNum,
-                    TotalNumItems = _BurialContext.Burials.Count()
-                }
+                    TotalNumItems = query.Count()
+                },
+                tasks = tasks
             }
                 );
         }
-                
+
+
+
+        [HttpPost]
+        public IActionResult Filter(string[] filter)
+        {
+            string id = string.Join('-', filter);
+            return RedirectToAction("BurialList", new { ID = id });
+        }
 
         [HttpGet]
         public IActionResult Add()
