5697bb36285ea1571083e6b7deaf6dd45a889f90
Preston Loveland <62196792+prestonloveland@users.noreply.github.com>
2021-04-12 09:23:16 -0600

    Added null to image field

--- Models/Filters.cs
+++ Models/Filters.cs
@@ -7,9 +7,10 @@ namespace BYUArchaeologyEgypt.Models
 {
     public class Filters
     {
+        //Constructor for filter class setting all necessary inputs to all
         public Filters(string filterstring)
         {
-            FilterString = filterstring ?? "all-all-all-all-all-";
+            FilterString = filterstring ?? "all-all-all-all-all- -all-all-all-all-all-all-all";
             string[] filters = FilterString.Split('-');
             HairColorId = filters[0];
             BurialWrapping = filters[1];
@@ -17,6 +18,13 @@ namespace BYUArchaeologyEgypt.Models
             Sex = filters[3];
             BonesCollected = filters[4];
             YearFound = filters[5];
+            LocationNS = filters[6];
+            LocationEW = filters[7];
+            LocationNSLowerValue = filters[8];
+            LocationNSUpperValue = filters[9];
+            LocationEWLowerValue = filters[10];
+            LocationEWUpperValue = filters[11];
+            LocationSubplot = filters[12];
         }
         public string FilterString { get; }
         public string HairColorId { get; }
@@ -26,12 +34,35 @@ namespace BYUArchaeologyEgypt.Models
         public string BonesCollected { get; }
         public string YearFound { get; }
 
+        //Location values to filter by
+        public string LocationNS { get; }
+        public string LocationEW { get; }
+        public string LocationNSLowerValue { get; }
+        public string LocationNSUpperValue { get; }
+        public string LocationEWLowerValue { get; }
+        public string LocationEWUpperValue { get; }
+        public string LocationSubplot { get; }
 
+
+
+
+
+        //Boolean values to check if the user has input something to filter by
         public bool HasHairColor => HairColorId.ToLower() != "all";
         public bool HasBurialWrapping => BurialWrapping.ToLower() != "all";
         public bool HasAgeBracket => AgeBracketAtDeath.ToLower() != "all";
         public bool HasSex => Sex.ToLower() != "all";
         public bool HasBonesCollected => BonesCollected.ToLower() != "all";
         public bool HasYearFound => YearFound.ToLower() != "";
+
+        //Sees if location filters are in place
+        public bool HasLocationNS => LocationNS.ToLower() != "all";
+        public bool HasLocationEW => LocationEW.ToLower() != "all";
+        public bool HasLocationNSUpper => LocationNSUpperValue.ToLower() != "all";
+        public bool HasLocationEWUpper => LocationEWUpperValue.ToLower() != "all";
+        public bool HasLocationNSLower => LocationNSLowerValue.ToLower() != "all";
+        public bool HasLocationEWLower => LocationEWLowerValue.ToLower() != "all";
+        public bool HasLocationSubplot => LocationSubplot.ToLower() != "all";
+
     }
 }
