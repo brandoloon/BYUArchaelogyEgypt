05bea09b446cf695f7e0b15c10bd25ca3e9006cb
fullme24 <true.fullmer@gmail.com>
2021-04-12 08:35:06 -0600

    migrations4/12/2021

---
+++ Migrations/20210412143236_AprilTw.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BYUArchaeologyEgypt.Migrations
+{
+    public partial class AprilTw : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Goods",
+                table: "Burials",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Goods",
+                table: "Burials",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string));
+        }
+    }
+}
