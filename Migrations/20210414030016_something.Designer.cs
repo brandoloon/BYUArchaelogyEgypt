09a82760f135f0fcc6090bfd40149460f07e60d1
Brandon Jacobsen <brandonerikjacobsen@gmail.com>
2021-04-13 21:18:02 -0600

    changes

--- Migrations/20210414030016_something.Designer.cs
+++ Migrations/20210414030016_something.Designer.cs
@@ -4,14 +4,16 @@ using BYUArchaeologyEgypt.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace BYUArchaeologyEgypt.Migrations
 {
     [DbContext(typeof(BurialContext))]
-    partial class BurialContextModelSnapshot : ModelSnapshot
+    [Migration("20210414030016_something")]
+    partial class something
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
